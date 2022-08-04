3ea5c8a25facb0eac967649b79edaa4ae0947eca
Anand Swaroop <anandswaroop191@gmail.com>
2022-08-04 11:50:44 -0400

    fix: define colors once

--- example/Main.hs
+++ example/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (unless)
-import Foreign (nullPtr, toBool, with)
+import Foreign (nullPtr, toBool, with, Ptr)
 import Foreign.C (newCString)
 import Raylib
   ( Color (Color),
@@ -15,26 +15,21 @@ import Raylib
     windowShouldClose,
   )
 
+foreign import ccall unsafe "bindings.h test" test :: Ptr Color -> IO()
+
 main = do
   str <- newCString "Hello world"
   initWindow 600 450 str
   setTargetFPS 60
-  gameLoop
+  with (Color 255 255 255 255) (with (Color 200 200 200 255) . gameLoop)
   closeWindow
 
-gameLoop = do
-  shouldClose <- windowShouldClose
+gameLoop :: Ptr Color -> Ptr Color -> IO ()
+gameLoop white gray = do
   beginDrawing
-  with
-    (Color 255 255 255 255)
-    ( \x -> do
-        clearBackground x
-        with
-          (Color 200 200 200 255)
-          ( \y -> do
-              str <- newCString "Testing Raylib"
-              drawText str 190 200 20 y
-              endDrawing
-              unless (toBool shouldClose) gameLoop
-          )
-    )
+  clearBackground white
+  str <- newCString "Testing Raylib"
+  drawText str 190 200 20 gray
+  endDrawing
+  shouldClose <- windowShouldClose
+  unless (toBool shouldClose) (gameLoop white gray)
