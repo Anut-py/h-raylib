7cefa5d3a5352de2be537f81fcf3195f2dd70c9d
Anand Swaroop <anandswaroop191@gmail.com>
2023-02-22 18:04:31 -0500

    fix: slight changes in run all script

--- run-all-examples.sh
+++ run-all-examples.sh
@@ -1,7 +1,18 @@
+set -e
+
+# core
 cabal run basic-window
-cabal run basic-images
-cabal run custom-font-text
-cabal run basic-audio
 cabal run first-person-camera
 cabal run camera-ray-collision
+
+# text
+cabal run custom-font-text
+
+# textures
+cabal run basic-images
+
+# models
 cabal run basic-models
+
+# audio
+cabal run basic-audio
