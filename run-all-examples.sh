2a09f3e2fd71df8e2c7d0f0e82044130eba1db2b
Anand Swaroop <anandswaroop191@gmail.com>
2023-02-24 16:14:22 -0500

    chore: added new example to run script

--- run-all-examples.sh
+++ run-all-examples.sh
@@ -1,18 +1,23 @@
 set -e
 
+cabal build
+
 # core
 cabal run basic-window
 cabal run first-person-camera
 cabal run camera-ray-collision
 
-# text
-cabal run custom-font-text
-
 # textures
 cabal run basic-images
 
+# text
+cabal run custom-font-text
+
 # models
 cabal run basic-models
+
+# shaders
+cabal run basic-shaders
 
 # audio
 cabal run basic-audio
