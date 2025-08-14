4173cd92c0bf428f8cd70279fbc0555c2edfed52
Anand Swaroop <anandswaroop191@gmail.com>
2025-08-14 18:58:29 -0400

    feat: add flag to disable lens

--- src/Raylib/Internal/TH.hs
+++ src/Raylib/Internal/TH.hs
@@ -3,58 +3,32 @@
 {-# LANGUAGE TupleSections #-}
 
 -- | Template Haskell functions used internally
-module Raylib.Internal.TH (genLenses, genNative) where
-
-import Control.Lens (makeLensesFor)
-import Control.Monad (zipWithM)
+module Raylib.Internal.TH (genNative) where
 
 #ifdef WEB_FFI
 
 import Language.Haskell.TH
   ( Body (NormalB),
     Clause (Clause),
-    Con (RecC),
-    Dec (DataD, FunD, SigD),
+    Dec (FunD, SigD),
     DecsQ,
     Exp (AppE, LitE, VarE),
-    Info (TyConI),
     Lit (StringL),
-    Name,
     TypeQ,
     mkName,
-    nameBase,
-    reify,
   )
 import Raylib.Internal.Web.Native (callRaylibFunction)
 
 #else
 
 import Language.Haskell.TH
-  ( Con (RecC),
-    Dec (DataD, ForeignD),
+  ( Dec (ForeignD),
     DecsQ,
-    Info (TyConI),
-    Name,
     TypeQ,
-    mkName,
-    nameBase,
-    reify, Foreign (ImportF), Callconv (CCall), Safety (Safe),
+    mkName, Foreign (ImportF), Callconv (CCall), Safety (Safe),
   )
 
 #endif
-
--- | Creates lenses with an underscore before field names; e.g. @vector2'x@
---   becomes the lens @_vector2'x@
-genLenses :: [Name] -> DecsQ
-genLenses names = do
-  infos <- mapM reify names
-  concat <$> zipWithM genLensesForType names infos
-  where
-    genLensesForType name (TyConI (DataD _ _ _ _ [RecC _ ctors] _)) =
-      makeLensesFor mapping name
-      where
-        mapping = map (\(a, _, _) -> let fName = nameBase a in (fName, '_' : fName)) ctors
-    genLensesForType _ _ = error "(genLenses) Received a name that does not refer to a valid type!"
 
 -- | Generates native code for the given functions. On non-web platforms, this
 --   means @foreign import@ statements. On web platforms, this means
